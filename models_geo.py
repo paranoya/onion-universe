f227d998fb144fe1eec5bfbc48b6f35e2bdedbd8
daacar <daniel.graciag@estudiante.uam.es>
2025-03-09 00:27:26 +0100

    temporary

--- models_geo.py
+++ models_geo.py
@@ -10,7 +10,7 @@ from astropy import constants as c
 # -----------------------------------------------------------------------------
 
 
-class CosmologicalModel(object):
+class CosmologicalModelGeometrical(object):
     
     def __init__(self,
                  Omega_k:u.dimensionless_unscaled,                 
@@ -19,11 +19,12 @@ class CosmologicalModel(object):
                  r_d:u.Mpc,
                 ):
         
-        # evolution
-        
+        # evolution. z(t) equivalent to a(t)
         self.t = t_sorted.decompose()
         self.z_t = z_inverse_sorted
         self.ln_a_t = -np.log(1+z_inverse_sorted)
+
+        self.Omega_k = Omega_k
         
         self.init_H_and_D()
 
@@ -48,16 +49,12 @@ class CosmologicalModel(object):
         self.Dc_t = c.c * np.hstack((Dc_t[0], Dc_t))
 
         DH = c.c / self.H0
-        #print('aaa', Omega_k, self.Ode0, self.Om0, self.Ogamma0, (1+self.neutrinos))
         if np.abs(self.Omega_k) < 1e-6:
-            #print(f'{Omega_k} is flat')
             self.Dm_t = self.Dc_t
         elif self.Omega_k < 0:
-            #print(f'{Omega_k} is closed')
             r = DH * np.abs(np.sin((self.Dc_t/DH).to_value(u.dimensionless_unscaled) * np.sqrt(-self.Omega_k)))
             self.Dm_t = r/np.sqrt(-self.Omega_k)
         else:
-            #print(f'{Omega_k} is open')
             r = DH * np.sinh((self.Dc_t/DH).to_value(u.dimensionless_unscaled) * np.sqrt(self.Omega_k))
             self.Dm_t = r/np.sqrt(self.Omega_k)
 
@@ -141,17 +138,6 @@ class CosmologicalModel(object):
 
 # -----------------------------------------------------------------------------
 
-
-class StandardModel(CosmologicalModel):
-
-    def __init__(self, model,
-                 r_d:u.Mpc,
-                 Y:u.dimensionless_unscaled=0.24):
-        z = np.logspace(-5, 3.99, 1001)[::-1]
-        super().__init__(model.Ode0, model.Om0, model.Ob0, Y, model.Ogamma0, model.Neff,
-                         model.age(z), z, model.Ode(z), model.Om(z), model.Ogamma(z),
-                         r_d)
-
 class FlatLCDM(StandardModel):
 
     def __init__(self,
@@ -161,7 +147,20 @@ class FlatLCDM(StandardModel):
         model = cosmo.FlatLambdaCDM(H0, 1-Ol0, Tcmb0=cosmo.Planck18.Tcmb0, Ob0=cosmo.Planck18.Ob0)
         super().__init__(model, r_d)
 
-# -----------------------------------------------------------------------------
+# --------------------------------------------------
+# ---------------------------
+class OnionModel(CosmologicalModelGeometrical)
+    def __init__(self,
+                 Ok:u.dimensionless_unscaled,
+                 H0:1/u.s,
+                 r_d:u.Mpc,
+                 k_st:u.dimensionless_unscaled,      #Generalized spacetime curvature to distinguish between onion models
+                ):
+
+        if np.abs(k_st) < 1e-6:
+            
+
+
 
 
 class Coasting(CosmologicalModel):
@@ -170,45 +169,17 @@ class Coasting(CosmologicalModel):
                  Ok:u.dimensionless_unscaled,
                  H0:1/u.s,
                  r_d:u.Mpc,
-                 Tcmb0:u.K=cosmo.Planck18.Tcmb0,
-                 Neff:u.dimensionless_unscaled=cosmo.Planck18.Neff,
-                 Ob0:u.dimensionless_unscaled=cosmo.Planck18.Ob0,
-                 Y:u.dimensionless_unscaled=0.24,
-                 eta:u.dimensionless_unscaled=6e-10,):
+                ):
 
-        # Present time
-        #print('t0', (1/H0).to_value(u.Gyr), 'Gyr')
 
-        ## photons
-        rho_gamma0 = 4*c.sigma_sb * Tcmb0**4 / c.c**3
-        Ogamma0 = (rho_gamma0 * 8*np.pi*c.G / 3/H0**2).to_value(u.dimensionless_unscaled)
 
-        ## masless neutrinos
-        neutrinos = (7/8)*Neff*(4/11)**(4/3)
-        Or0 = Ogamma0 * (1 + neutrinos)
-
-        ## (non-relativistic) matter
-        mu0 = ((1 - Ok) / Or0 - 3) / 2
-        t0_teq = np.sqrt(5*mu0**4 / (3+2*mu0))
-        #print('Omega_r', Or0, Ogamma0*(1+neutrinos))
-
-        #print('mu, zeq', mu0, t0_teq - 1)
-
-        Om0 = mu0 * Or0
-        rho_m0 = Om0 * 3*H0**2/(8*np.pi*c.G)
-        #print('Omega_m', Om0, mu0 / (3 + 2 * mu0) * (1-Ok))
-
-        n_gamma0 = 16*np.pi*zeta(3) * (c.k_B*Tcmb0 / c.h/c.c)**3
-        rho_b0 = eta * c.m_p * n_gamma0
-        Ob0 = (rho_b0 * 8*np.pi*c.G / 3/H0**2).to_value(u.dimensionless_unscaled)
-        #print('Omega_b', Ob0)
 
         # Evolution
 
         mu = mu0 * np.hstack((.5*np.logspace(-4, -1e-4, 1001),
-                              (1 - .5*np.logspace(-6, -1e-4, 1001))[::-1]))
+                              (1 - .5*np.logspace(-6, -1e-4, 1001))[::-1]))    
 
-        t_teq = np.sqrt(5*mu**4 / (3+2*mu))
+
 
         z = t0_teq / t_teq - 1
         t = 1/H0 / (1+z)
@@ -218,12 +189,7 @@ class Coasting(CosmologicalModel):
         Ob = Ob0/Om0 * Om
         Ode = 2*Or + Om
 
-        super().__init__(2*Or0+Om0, Om0, Ob0, Y, Ogamma0, Neff,
-                         t, z, Ode, Om, Or/(1+neutrinos),
-                         r_d)
+        super().__init__(Ok, t, z, r_d)
 
 # -----------------------------------------------------------------------------
 
-
-flcdm = FlatLCDM(Ol0=cosmo.Planck18.Ode0, H0=cosmo.Planck18.H0, r_d=140*u.Mpc)
-coasting = Coasting(Ok=-.05, H0=70*u.km/u.s/u.Mpc, r_d=140*u.Mpc, Tcmb0=cosmo.Planck18.Tcmb0, Neff=cosmo.Planck18.Neff, Ob0=cosmo.Planck18.Ob0)
